f929c5328e3839e9a60470da3e254e1d2296e6cc
root <zxe1711@outlook.com>
2023-12-08 12:01:34 +0800

    simple test

---
+++ main.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from model import Model
+loss=nn.CrossEntropyLoss()
+def train(model, optimizer, dataloader, loss_function):
+    model.train()  # set the model to training mode
+    total_loss = 0
+
+    for batch in dataloader:
+        inputs = batch["input"]
+        gt = batch["gt"]
+
+        # Forward pass: compute the model output
+        output = model(inputs,gt)
+
+        # Compute the loss
+        train_loss = loss_function(output, gt[:,1:])
+
+        # Backward pass: compute gradient of the loss with respect to model parameters
+        optimizer.zero_grad()  # clear previous gradients
+        train_loss.backward()  # backpropagation
+
+        # Update parameters
+        optimizer.step()
+
+        total_loss += train_loss.item()
+
+    average_loss = total_loss / len(dataloader)
+    return average_loss
---
+++ model.py
@@ -0,0 +1,20 @@
+from transformers import ResNetConfig,ResNetModel
+from torch import nn  
+from torch.nn import TransformerDecoder,TransformerDecoderLayer
+class Model(nn.modules):
+     
+    def __init__(self,in_ch,embed_size,hidden_size,depth,de_d_model,de_numhead,numlayer,type='full'):
+        super(nn.modules, self).__init__()
+        self.resconfig=ResNetConfig(num_channels=in_ch,embedding_size=embed_size,hidden_sizes=hidden_size,depths=depth)
+        self.encoder=ResNetModel(self.resconfig)
+        self.decoderlayer=TransformerDecoderLayer(d_model=de_d_model,nhead=de_numhead)
+        self.decoder=TransformerDecoder(self.decoder,num_layers=numlayer)
+        self.type=type
+    def forward(self,input,gt):
+        hidden=self.encoder(input)
+        if self.type=="full":
+            output=self.decoder(hidden.pooled_output,gt[:,:-1])
+        elif  self.type=="patch":
+            hidden=hidden.last_hidden_state.view(hidden.shape[0],hidden.shape[1],-1).permute(0,2,1)
+            output=self.decoder(hidden,gt[:,:-1])
+        return output
